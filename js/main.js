e761790d4ef754750d6cd4ee7ff5a6a907952136
mizuha2020 <anhquoca5hht@gmail.com>
2021-09-07 15:27:29 +0700

    them danh muc, fix linh tinh

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const newNotify = document.querySelectorAll(".notify-info-new");
 const numOfNotify = document.querySelector(".notify-number");
+const navbarUser = document.querySelectorAll(".navbar__menu")[1];
 numOfNotify.innerText = newNotify.length;
 newNotify.forEach(function(item) {
     item.addEventListener("click", function(e) {
-        e.target.parentNode.classList.toggle("notify-info-new");
+        e.target.parentNode.classList.remove("notify-info-new");
         let newNotify = document.querySelectorAll(".notify-info-new");
         if(newNotify.length > 0){
             numOfNotify.innerText = newNotify.length;
@@ -15,4 +16,18 @@ newNotify.forEach(function(item) {
         console.log(newNotify);
 
     })
+})
+const logOut = document.getElementById("log-out");
+logOut.addEventListener("click", function(e){
+    navbarUser.classList.remove("menu-user")
+})
+const userInfo = document.getElementById("user-info");
+userInfo.addEventListener("click", function(e){
+    const userName = document.getElementById("user-name");
+    const userAvt = document.querySelector(".navbar__menu__user__avt");
+    console.log(userName);
+    console.log(userAvt);
+    navbarUser.classList.add("menu-user")
+    userName.innerText= "Khoa";
+    userAvt.src = "https://scontent.xx.fbcdn.net/v/t1.6435-1/p100x100/52161636_1064694103715214_75803037448273920_n.jpg?_nc_cat=108&ccb=1-5&_nc_sid=dbb9e7&_nc_ohc=OwLo2wdhXuoAX8ncaMf&_nc_ad=z-m&_nc_cid=0&_nc_ht=scontent.xx&oh=b5057126446a9061dd2660dd74cd1ef9&oe=615D4554";
 })
